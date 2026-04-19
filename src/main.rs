cfde5535015fc9b4edf9e56a8f6c125c56c8a066
Reo q <reoq@MacBook-Pro-de-Reo.local>
2026-04-19 02:56:11 -0500

    Added some notes on main.rs in src

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/* First Hello, world. 
+Ok, so. Let's be clear, there's a main function, this function is the first code line that will always run in any Rust project. In this excercise it's empty, it means that this main funcion by itself 
+won't return anything. 
+
+In second place we have "println!" function inside of the body of main function, this makes everything in this example: print text on screen. 
+
+And that's everything, but before forget something till I know this "!" calls a macro, by the moment I don't know what does it mean. I know macros on videogames, but I don't know if they 
+similar or something.
+
+
+ */
